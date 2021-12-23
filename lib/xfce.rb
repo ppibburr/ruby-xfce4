bd8aee9da19c2bfa0f9c16686c498ca2b29ab2d6
ppibburr <tulnor33@gmail.com>
2021-12-23 01:44:26 -0500

    ...

--- lib/xfce.rb
+++ lib/xfce.rb
@@ -2,18 +2,29 @@ require 'gtk3'
 
 module Xfce
   Loader = GObjectIntrospection::Loader.new(Xfce)
-  l = "L"
+  l      = "L"
+  
   begin
     Loader.load("#{l}ibxfce4panel")
-    Loader.load("#{l}ibxfce4util")
-    #Loader.load("#{l}ibxfce4ui")  
+    
+    begin
+      ## These are
+      ## not required
+      Loader.load("#{l}ibxfce4util")
+      #Loader.load("#{l}ibxfce4ui")  
+    rescue => e
+    end
   rescue => e
     raise e if l == 'l'
     l = 'l'
     retry
   end
   def self.init_plugin ptr=nil, &b
-    return @plugin = GObjectIntrospection::Loader.instantiate_gobject_pointer(ptr) if !b && ptr
+    if !b && ptr
+      @plugin = GObjectIntrospection::Loader.instantiate_gobject_pointer(ptr) 
+      return @plugin
+    end
+    @plugin.children[0].destroy
     b.call @plugin if @plugin && b
   end
 end
